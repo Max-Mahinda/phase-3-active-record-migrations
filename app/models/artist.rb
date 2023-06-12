660b715d5b42046c08d96015f5ec29b126da30e8
Max <maxmwangikenya@gmail.com>
2023-06-12 14:20:50 +0300

    minor changes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,10 @@
-# add the Artist class here
+class Artist < ActiveRecord::Base
+    # Define attributes and validations
+    attr_accessor :name, :genre, :age, :hometown
+  
+    # Define a migration to add a favorite_flower column to the artists table
+    def change
+      add_column :artists, :favorite_flower, :string
+    end
+  end
+  
